78977ab1188a5700fa51e256ec68cc5465a6c85b
A1079146598 <3084447185@qq.com>
2020-09-14 01:54:54 +0800

    c4

--- scrapy_project/tieba_1/tieba_1/pipelines.py
+++ scrapy_project/tieba_1/tieba_1/pipelines.py
@@ -7,7 +7,23 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import pymysql
 
 class Tieba1Pipeline:
+    def __init__(self):
+        try:
+            self.conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='914673123', db='xiaoshuo')
+            self.cur = self.conn.cursor()
+        except Exception as e:
+            print(e)
+
     def process_item(self, item, spider):
+        strsql = 'insert into tieba_scr VALUES (0,%s,%s,%s,%s)'
+        params = [item['title'], item['answer_count'], item['author'], item['url_t']]
+        self.cur.execute(strsql, params)
+        self.conn.commit()
         return item
+
+    def close_spider(self, spdier):
+        self.cur.close()
+        self.conn.close()
